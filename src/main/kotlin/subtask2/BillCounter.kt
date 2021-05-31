5f199f75508aa0c45727e088d61c02ef42db463d
kirillturok <71722037+kirillturok@users.noreply.github.com>
2021-05-31 15:10:18 +0300

    Update BillCounter.kt

--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -5,6 +5,16 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
+        var sum=0
+        for(i in 0..k-1){
+            sum+=bill[i]
+        }
+        for (i in k+1..bill.size-1){
+            sum+=bill[i]
+        }
+        sum/=2
+        if(sum==b) return "Bon Appetit"
+        return (b-sum).toString()
         throw NotImplementedError("Not implemented")
     }
 }
